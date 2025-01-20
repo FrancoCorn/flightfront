deda50b56ac84dd92b98b7deae51540c52b541d7
FrancoCorn <fcorn@fi.uba.ar>
2025-01-20 19:38:42 -0300

    Agrego filtros por categorias

--- app/(tabs)/schools.tsx
+++ app/(tabs)/schools.tsx
@@ -9,12 +9,23 @@ type Aeroclub = {
   provincia: string;
 };
 
+const categories = [
+  { id: 1, name: 'Planeador' },
+  { id: 2, name: 'Avión privado' },
+  { id: 3, name: 'Avión comercial' },
+  { id: 6, name: 'Fuerzas armadas' },
+  { id: 7, name: 'Paracaídas' },
+  { id: 4, name: 'Helicóptero privado' },
+  { id: 5, name: 'Helicóptero comercial' },
+];
+
 export default function TabSchoolsScreen() {
   const [aeroclubs, setAeroclubs] = useState<Aeroclub[]>([]);
   const [loading, setLoading] = useState(true);
   const [search, setSearch] = useState('');
   const [filteredAeroclubs, setFilteredAeroclubs] = useState<Aeroclub[]>([]);
   const [modalVisible, setModalVisible] = useState(false);
+  const [selectedFilters, setSelectedFilters] = useState<{ [key: string]: boolean }>({});
 
   useEffect(() => {
     fetch('http://127.0.0.1:8080/api/aeroclubes/')
@@ -42,6 +53,18 @@ export default function TabSchoolsScreen() {
   if (loading) {
     return (
       <View style={styles.container}>
+              <Text style={styles.title}>Escuelas de vuelo</Text>
+              <View style={styles.searchContainer}>
+                <TextInput
+                  style={styles.searchInput}
+                  placeholder="Buscar por nombre o provincia"
+                  value={search}
+                  onChangeText={setSearch}
+                />
+                <TouchableOpacity style={styles.filterButton} onPress={() => setModalVisible(true)}>
+                  <Icon name="filter" size={24} color="#000" />
+                </TouchableOpacity>
+              </View>
         <ActivityIndicator size="large" color="#fff" />
       </View>
     );
@@ -72,15 +95,76 @@ export default function TabSchoolsScreen() {
         )}
       />
       <Modal
-        animationType="slide"
+        animationType="fade"
         transparent={true}
         visible={modalVisible}
         onRequestClose={() => setModalVisible(false)}
       >
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
-            <Text style={styles.modalTitle}>Filtros</Text>
-            {/* Aquí puedes agregar los filtros que desees */}
+            <Text style={styles.modalTitle}>Categorias</Text>
+            {categories.map((category) => (
+              <View key={category.id} style={styles.filterItem}>
+                <TouchableOpacity
+                  style={styles.checkbox}
+                  onPress={() => {
+                    setSelectedFilters((prevFilters) => ({
+                      ...prevFilters,
+                      [category.id]: !prevFilters[category.id],
+                    }));
+                  }}
+                >
+                  {selectedFilters[category.id] ? (
+                    <Icon name="checkbox" size={24} color='#1F2C37' />
+                  ) : (
+                    <Icon name="square-outline" size={24} color='#1F2C37' />
+                  )}
+                </TouchableOpacity>
+                <Text style={styles.filterText}>{category.name}</Text>
+              </View>
+            ))}
+            <TouchableOpacity
+              style={styles.applyButton}
+              onPress={() => {
+                setLoading(true);
+                const selectedCategoryIds = Object.keys(selectedFilters)
+                  .filter((categoryId) => selectedFilters[categoryId])
+                  .join(",");
+                
+                if (selectedCategoryIds) {
+                  
+                  fetch(`http://127.0.0.1:8080/aeroclubes/get_aeroclubs_with_category/${selectedCategoryIds}/`)
+                    .then((response) => response.json())
+                    .then((data) => {
+                      setFilteredAeroclubs(data);
+                      setLoading(false);
+                    })
+                    .catch((error) => {
+                      console.error("Error fetching aeroclubs:", error);
+                      
+                    });
+                } else{
+                  
+                  fetch('http://127.0.0.1:8080/api/aeroclubes/')
+                  .then((response) => response.json())
+                  .then((data) => {
+                    setAeroclubs(data);
+                    setFilteredAeroclubs(data);
+                    setLoading(false);
+                    
+                  })
+                  .catch((error) => {
+                    console.error('Error fetching aeroclubs:', error);
+                    
+                  });
+                }
+                
+                setModalVisible(false);
+              }}
+              
+            >
+              <Text style={styles.applyButtonText}>Aplicar</Text>
+            </TouchableOpacity>
             <TouchableOpacity onPress={() => setModalVisible(false)}>
               <Text style={styles.closeButton}>Cerrar</Text>
             </TouchableOpacity>
@@ -122,11 +206,10 @@ const styles = StyleSheet.create({
   },
   aeroclubContainer: {
     backgroundColor: '#a5a5a5',
-    borderRadius: 20,
+    borderRadius: 5,
     paddingLeft: 130,
     paddingRight: 130,
     marginVertical: 10,
-    alignItems: 'center',
     justifyContent: 'center',
   },
   aeroclubName: {
@@ -152,7 +235,7 @@ const styles = StyleSheet.create({
   },
   modalContent: {
     width: '80%',
-    backgroundColor: '#fff',
+    backgroundColor: '#a5a5a5',
     borderRadius: 10,
     padding: 20,
     alignItems: 'center',
@@ -161,9 +244,34 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 20,
+    color: '#000',
+  },
+  filterItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+    width: '100%', 
+  },
+  checkbox: {
+    marginRight: 10,
+    
+  },
+  filterText: {
+    fontSize: 16,
+    color: '#000',
   },
   closeButton: {
     marginTop: 20,
-    color: '#007AFF',
+    color: '#1F2C37',
+  },
+  applyButton: {
+    backgroundColor: "#1F2C37",
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  applyButtonText: {
+    color: "#fff",
+    fontSize: 16,
   },
 });
